bbbc8bbda413aa17a9ff867c7d5180c0beb1a62e
OneTwo33 <alex_tsai@mail.ru>
2023-01-01 15:26:12 +0600

    Solved problem N+1 (many queries from categories) when show all products

--- shop-database/src/main/java/ru/onetwo33/persist/ProductRepository.java
+++ shop-database/src/main/java/ru/onetwo33/persist/ProductRepository.java
@@ -1,7 +1,10 @@
 package ru.onetwo33.persist;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import ru.onetwo33.persist.model.Product;
 
 import java.math.BigDecimal;
@@ -9,6 +12,9 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
 
+    @Query(value = "select p from Product p left join fetch p.category",
+            countQuery = "select count(p) from Product p")
+    Page<Product> findAll(Pageable page);
     List<Product> findProductByNameLike(String name);
 
     List<Product> findByPriceBetween(BigDecimal min, BigDecimal max);
